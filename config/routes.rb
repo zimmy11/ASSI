8cbd32bf59d63675f92afcc98def14841a11e819
NNab01 <nobili.1954804@studenti.uniroma1.it>
2023-06-30 18:51:52 +0200

    versione ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: "users/registrations",
-    sessions: 'users/sessions'
+    registrations: "users/registrations"
   }
+  
   
   
   resources :bans
